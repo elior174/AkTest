d3248fef6607b802087ec73e32876d0a55c1a803
elior174 <eliordadon@walla.com>
2022-12-24 23:49:05 +0200

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -8,7 +8,7 @@ terraform {
 
 
 resource "aws_iam_role" "eks-iam-role" {
- name = "devopsthehardway-eks-iam-role"
+ name = "aktest-eks-iam-role"
 
  path = "/"
 
@@ -38,8 +38,8 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EK
  role    = aws_iam_role.eks-iam-role.name
 }
 
-resource "aws_eks_cluster" "devopsthehardway-eks" {
- name = "devopsthehardway-cluster"
+resource "aws_eks_cluster" "aktest-eks" {
+ name = "aktest-cluster"
  role_arn = aws_iam_role.eks-iam-role.arn
 
  vpc_config {
@@ -89,8 +89,8 @@ resource "aws_iam_role" "workernodes" {
 
 
 resource "aws_eks_node_group" "worker-node-group" {
-  cluster_name  = aws_eks_cluster.devopsthehardway-eks.name
-  node_group_name = "devopsthehardway-workernodes"
+  cluster_name  = aws_eks_cluster.aktest-eks.name
+  node_group_name = "aktest-workernodes"
   node_role_arn  = aws_iam_role.workernodes.arn
   subnet_ids   = [var.subnet_id_1, var.subnet_id_2]
   instance_types = ["t3.xlarge"]
